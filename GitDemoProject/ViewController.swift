612cbd5fb52a970ec68c7aed5010963a334fb46c
SONALI PATEL <patelsonali123@yahoo.com>
2018-05-23 19:30:59 -0400

    Reverted Changes

--- GitDemoProject/ViewController.swift
+++ GitDemoProject/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello CS193p")
-        print("Hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
